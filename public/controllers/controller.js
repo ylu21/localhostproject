1d2d7d688344f44277b6e13d4de67e59876b81d2
ylu21 <ylu21@u.rochester.edu>
2015-11-23 23:13:40 -0500

    initial commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,14 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('AppCtrl',['$scope', '$http', function($scope, $http){
+    console.log("Hellow world from controller");
+    
+    $http.get('/').success(function(res){
+        console.log("i got the requested data");
+    });
+    
+//    $scope.addSeat = function(){
+//      console.log($scope.seat);   
+//    };
+//    $scope.seat = 
+    
+}]);
